35d3abb2683fa6c2c42de21f14d7bce37bd1d4b4
huangyingwen <huangyingwen@caicloud.io>
2020-09-16 10:27:03 +0800

    fix: 组件必须有名称

--- portal/src/components/root.js
+++ portal/src/components/root.js
@@ -1,28 +1,30 @@
 import { Link } from 'react-router-dom';
 
-export default ({ children }) => (
-  <div>
-    <h1>Root</h1>
-    <ul>
-      <li>
-        <Link to="/">home</Link>
-      </li>
-      <li>
-        <Link to="/configs">configs</Link>
-      </li>
-      <li>
-        <Link to="/app-home">app-home</Link>
-      </li>
-      <li>
-        <Link to="/app-configs">app-configs</Link>
-      </li>
-      <li>
-        <Link to="/devops-home">devops-home</Link>
-      </li>
-      <li>
-        <Link to="/devops-configs">devops-configs</Link>
-      </li>
-    </ul>
-    {children}
-  </div>
-);
+export default function Root({ children }) {
+  return (
+    <div>
+      <h1>Root</h1>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/configs">configs</Link>
+        </li>
+        <li>
+          <Link to="/app-home">app-home</Link>
+        </li>
+        <li>
+          <Link to="/app-configs">app-configs</Link>
+        </li>
+        <li>
+          <Link to="/devops-home">devops-home</Link>
+        </li>
+        <li>
+          <Link to="/devops-configs">devops-configs</Link>
+        </li>
+      </ul>
+      {children}
+    </div>
+  );
+}
